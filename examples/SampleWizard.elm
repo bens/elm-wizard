1ad0b2f7402501771b638d9ac3980c4826792ac2
Ben Sinclair <ben.d.sinclair@gmail.com>
2016-07-11 00:43:33 +1000

    Improved API and names, added better docs

--- examples/SampleWizard.elm
+++ examples/SampleWizard.elm
@@ -1,6 +1,8 @@
 module SampleWizard exposing (Msg, Model, WizardModel, init, restart, update, view)
 
+import Cons exposing (Cons)
 import Html exposing (Html)
+import Html.App as App
 import Html.Attributes as Attr
 import Html.Events exposing (onClick, onInput)
 import String
@@ -51,7 +53,7 @@ vvalid v =
 
 wizard : Model -> Wizard SError SMsg Model
 wizard =
-  W.wizard (W.defaultView True) stepA [stepB]
+  W.wizard (viewWizard True) (Cons.cons stepA [stepB])
 
 
 { init, update, view } =
@@ -112,3 +114,57 @@ stepB =
       check (Model _ x) =
         if vvalue x > 0 then W.Ok else W.Failed [Zero]
   in { update = update, view = view, check = check }
+
+
+viewWizard : Bool -> W.ViewModel model -> Html msg -> Html (W.Msg err msg)
+viewWizard showDebug m body =
+  let empty = Html.text "○"
+      full = Html.text "●"
+      html =
+        [ Html.p [ Attr.class "wizard-step"
+                 , Attr.style [("width", "15em"), ("height", "5em")]
+                 , border
+                 , spacing 1
+                 ]
+            [App.map W.embedMsg body]
+        , Html.span [ spacing 0.5 ]
+            [ Html.button [spacing 0.2, onClick W.back]
+                [Html.text (if m.currentStep == 0 then "Cancel" else "Back ⇐")]
+            , Html.span [spacing 0.2]
+                (List.repeat m.currentStep empty ++
+                   [full] ++
+                   List.repeat (m.numSteps - m.currentStep - 1) empty)
+            , Html.button [ spacing 0.2
+                          , onClick W.forward
+                          ]
+                [Html.text (if m.currentStep == m.numSteps - 1 then "Finish" else "⇒ Next")]
+            ]
+        ]
+      debug =
+        [ Html.p [ Attr.class "wizard-debug"
+                 , Attr.style [ ("width", "15em")
+                              , ("background-color", "#eeeeee")
+                              ]
+                 , border
+                 , spacing 1
+                 ]
+            [ Html.text ("DEBUG: " ++ toString m.model) ]
+        ]
+  in
+  Html.div [ Attr.class "wizard"
+           , border
+           , spacing 1
+           ]
+    (html ++ (if showDebug then debug else []))
+
+
+spacing : Float -> Html.Attribute msg
+spacing n =
+  Attr.style [ ("margin", toString n ++ "em")
+             , ("padding", "0.5em")
+             ]
+
+
+border : Html.Attribute msg
+border =
+  Attr.style [ ("border", "thin lightgrey solid") ]
